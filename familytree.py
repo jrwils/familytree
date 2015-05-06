ef7cb3565e5ff85320657d438dd2be1dbba5c864
Joe Wilson <joe.wlsn@gmail.com>
2015-05-06 09:59:20 -0700

    Adds an add_child method to add children of a parent

--- familytree.py
+++ familytree.py
@@ -8,3 +8,22 @@ class FamilyMember:
         self.name = name
         self.parent = None
         self.children = []
+
+    def add_child(self, parent, name):
+        '''
+        This method adds a FamilyMember object to the children
+        array of the parent when passed a parent name.
+        It recursively goes through the tree to find a match of
+        the parent's name. Returns True if the parent is found and
+        the child object is created. Returns False if the parent is not
+        found and no child object is created.
+        '''
+        current_member = self
+        if current_member.name == parent:
+            child = FamilyMember(name)
+            child.parent = current_member
+            current_member.children.append(child)
+            return True
+        for child in current_member.children:
+            return child.add_child(parent, name)
+        return False
