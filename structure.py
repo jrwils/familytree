007f701f141685e6d343ca88f8ea66eeacca9dc2
Joe Wilson <joe.wlsn@gmail.com>
2015-05-06 11:38:12 -0700

    Adds a function for creating the example tree structure

---
+++ structure.py
@@ -0,0 +1,18 @@
+from familytree import FamilyMember
+
+def create_tree_structure():
+    tree_root = FamilyMember('Nancy')
+    tree_root.add_child('Nancy', 'Adam')
+    tree_root.add_child('Nancy', 'Jill')
+    tree_root.add_child('Nancy', 'Carl')
+    tree_root.add_child('Carl', 'Catherine')
+    tree_root.add_child('Carl', 'Joseph')
+    tree_root.add_child('Jill', 'Kevin')
+    tree_root.add_child('Kevin', 'Samuel')
+    tree_root.add_child('Kevin', 'George')
+    tree_root.add_child('Kevin', 'James')
+    tree_root.add_child('Kevin', 'Aaron')
+    tree_root.add_child('James', 'Mary')
+    tree_root.add_child('George', 'Patrick')
+    tree_root.add_child('George', 'Robert')
+    return tree_root
